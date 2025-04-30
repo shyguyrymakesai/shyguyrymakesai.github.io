fdce321ebead0c31fd45c7afcb0fb0b92d818b08
shyguyrymakesai <ryan.martinez2@protonmail.com>
2025-04-30 18:42:27 -0400

    Fixed Tailwind!

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-site/'  // use '/' if repo is ryanmartinez.github.io
+  base: '/', // ✅ this is correct for user pages
 });
